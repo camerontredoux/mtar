887e99d47741b9fee79420b17a21580e55e962a0
Cameron Tredoux <camtredoux@gmail.com>
2022-01-31 17:01:52 -0700

    changed macros to regular functions, more readability (same with switch back to if)

--- myTar.cpp
+++ myTar.cpp
@@ -6,27 +6,32 @@
 #include <sys/stat.h>
 #include <sys/time.h>
 #include <unistd.h>
+
 using namespace std;
 
-#define help()                                                                 \
-  printf("myTar:\t-a: archive\n\t\tfile.mtar: name of archive "                \
-         "file\n\t\tfile1: must have at least one file to "                    \
-         "archive\n\t\tfiles: optional additional files to archive\n\t-x: "    \
+void help() {
+  printf("myTar:\t-a: archive\n\t\tfile.mtar: name of archive file\n\t\tfile1: "
+         "must have at least one file to "
+         "archive\n\t\tfiles: optional additional files to archive\n\t-x: "
          "extract\n\t\tfile.mtar: archived file to extract files from\n\n");
+}
 
-#define usage_err()                                                            \
-  fprintf(stderr, "myTar:\tarchive: ./myTar -a file.mtar file1 "               \
+void usage_err() {
+  fprintf(stderr, "myTar:\tarchive: ./myTar -a file.mtar file1 "
                   "[files...]\n\textract: ./myTar -x file.mtar\n\n");
+}
 
-#define file_err(TYPE)                                                         \
-  fprintf(stderr, "myTar: \t%s: incorrect file extension\n\n", TYPE);
+void file_err(const char *type) {
+  fprintf(stderr, "myTar: \t%s: incorrect file extension\n\n", type);
+}
 
 void archive(const char *fileName) {
   const char *fileNameExtension = strrchr(fileName, '.');
   if (!fileNameExtension || strcmp(fileNameExtension, ".mtar") != 0) {
     file_err("archive");
-    exit(EXIT_FAILURE);
+    exit(1);
   }
+
   FILE *fptr = fopen(fileName, "w");
   struct stat sb;
   stat(fileName, &sb);
@@ -39,7 +44,7 @@ void extract(const char *fileName) {
   const char *fileNameExtension = strrchr(fileName, '.');
   if (!fileNameExtension || strcmp(fileNameExtension, ".mtar") != 0) {
     file_err("extract");
-    exit(EXIT_FAILURE);
+    exit(1);
   }
   FILE *fptr = fopen(fileName, "r");
   if (fptr) {
@@ -50,38 +55,32 @@ void extract(const char *fileName) {
 int main(int argc, char **argv) {
   if (argc < 2) {
     usage_err();
-    exit(EXIT_FAILURE);
+    exit(1);
   }
 
   if (strcmp(argv[1], "--help") == 0) {
     help();
-    exit(EXIT_SUCCESS);
+    exit(0);
   }
 
-  if (argv[1][0] == '-') {
-    switch (argv[1][1]) {
-    case 'a':
-      if (argc < 3) {
-        fprintf(stderr, "myTar:\tarchive: ./myTar -a file.mtar file1 "
-                        "[files...]\n\n");
-        exit(EXIT_FAILURE);
-      }
-      archive(argv[2]);
-      break;
-    case 'x':
-      if (argc < 3) {
-        fprintf(stderr, "myTar:\textract: ./myTar -x file.mtar\n\n");
-        exit(EXIT_FAILURE);
-      }
-      extract(argv[2]);
-      break;
-    case 'h':
-      help();
-      exit(EXIT_SUCCESS);
-      break;
-    default:
-      usage_err();
-      exit(EXIT_FAILURE);
+  if (strcmp(argv[1], "-a") == 0) {
+    if (argc < 3) {
+      fprintf(stderr,
+              "myTar:\tarchive: ./myTar -a file.mtar file1 [files...]\n\n");
+      exit(1);
     }
+    archive(argv[2]);
+  } else if (strcmp(argv[1], "-x") == 0) {
+    if (argc < 3) {
+      fprintf(stderr, "myTar:\textract: ./myTar -x file.mtar\n\n");
+      exit(1);
+    }
+    extract(argv[2]);
+  } else if (strcmp(argv[1], "-h") == 0 || strcmp(argv[1], "--help") == 0) {
+    help();
+    exit(0);
+  } else {
+    usage_err();
+    exit(1);
   }
 }
