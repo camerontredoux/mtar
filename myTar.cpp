fab1258f9e1d6ea34b1c6e61a6fca81b21705a2b
Cameron Tredoux <camtredoux@gmail.com>
2022-02-02 15:37:53 -0700

    successfully extracts all files from mtar file

--- myTar.cpp
+++ myTar.cpp
@@ -1,3 +1,4 @@
+#include <errno.h>
 #include <fcntl.h>
 #include <stdio.h>
 #include <stdlib.h>
@@ -14,40 +15,33 @@ using namespace std;
 #define st_mtim st_mtimespec
 #endif
 
-void help()
-{
+void help() {
   printf("myTar:\t-a: archive\n\t\tfile.mtar: name of archive file\n\t\tfile1: "
          "must have at least one file to "
          "archive\n\t\tfiles: optional additional files to archive\n\t-x: "
          "extract\n\t\tfile.mtar: archived file to extract files from\n\n");
 }
 
-void usage_err()
-{
+void usage_err() {
   fprintf(stderr, "myTar:\tarchive: ./myTar -a file.mtar file1 "
                   "[files...]\n\textract: ./myTar -x file.mtar\n\n");
 }
 
-void file_err(const char *type)
-{
+void file_err(const char *type) {
   fprintf(stderr, "myTar: \t%s: incorrect file extension\n\n", type);
 }
 
-void check_extension(const char *fileName)
-{
+void check_extension(const char *fileName) {
   const char *fileNameExtension = strrchr(fileName, '.');
-  if (!fileNameExtension || strcmp(fileNameExtension, ".mtar") != 0)
-  {
+  if (!fileNameExtension || strcmp(fileNameExtension, ".mtar") != 0) {
     file_err("archive");
     exit(1);
   }
 }
 
-char *read_file(FILE *fptr, off_t size)
-{
+char *read_file(FILE *fptr, off_t size) {
   char *file_content = (char *)malloc(sizeof(char) * size);
-  if (fread(file_content, sizeof(char), size, fptr) < size)
-  {
+  if (fread(file_content, sizeof(char), size, fptr) < size) {
     perror("fread() file content");
     exit(1);
   }
@@ -55,24 +49,20 @@ char *read_file(FILE *fptr, off_t size)
   return file_content;
 }
 
-void archive(const char *fileName, FILE *mtarptr)
-{
+void archive(const char *fileName, FILE *mtarptr) {
   FILE *fptr = fopen(fileName, "r");
-  if (!fptr)
-  {
+  if (!fptr) {
     perror("fopen()");
     exit(1);
   }
 
   struct stat sb;
-  if (stat(fileName, &sb) == -1)
-  {
+  if (stat(fileName, &sb) == -1) {
     perror("stat()");
     exit(1);
   }
 
-  if (S_ISREG(sb.st_mode))
-  {
+  if (S_ISREG(sb.st_mode)) {
     int fileName_length = strlen(fileName);
     fwrite(&fileName_length, sizeof(int), 1, mtarptr);
     fwrite(&sb, sizeof(struct stat), 1, mtarptr);
@@ -81,11 +71,11 @@ void archive(const char *fileName, FILE *mtarptr)
     char *file_content = read_file(fptr, sb.st_size);
     fwrite(file_content, sizeof(char), sb.st_size, mtarptr);
 
+    printf("archived %s (%lu bytes)\n", fileName, sb.st_size);
+
     free(file_content);
     fclose(fptr);
-  }
-  else
-  {
+  } else {
     fprintf(
         stderr,
         "myTar:\tarchive: incorrect file type (must be a regular file)\n\n");
@@ -93,66 +83,59 @@ void archive(const char *fileName, FILE *mtarptr)
   }
 }
 
-void extract(const char *mtarname)
-{
+void extract(const char *mtarname) {
   FILE *mtarptr = fopen(mtarname, "r");
-  if (!mtarptr)
-  {
+  if (!mtarptr) {
     perror("fopen()");
     exit(1);
   }
 
-  struct stat sb;
+  int count = 0;
+  while (1) {
+    struct stat sb;
 
-  int fileName_length;
-  if (fread(&fileName_length, sizeof(int), 1, mtarptr) < 1)
-  {
-    perror("fread() filename length");
-    exit(1);
-  }
-  if (fread(&sb, sizeof(struct stat), 1, mtarptr) < 1)
-  {
-    perror("fread() stat buffer");
-    exit(1);
-  }
-  char *fileName = read_file(mtarptr, fileName_length);
-  char *file_content = read_file(mtarptr, sb.st_size);
+    int fileName_length;
+    if (fread(&fileName_length, sizeof(int), 1, mtarptr) < 1) {
+      // if EOF, success
+      if (errno == 0) {
+        printf("myTar:\textract: success\n");
+        exit(0);
+      }
+      perror("fread() filename length");
+      exit(1);
+    }
+    if (fread(&sb, sizeof(struct stat), 1, mtarptr) < 1) {
+      perror("fread() stat buffer");
+      exit(1);
+    }
+    char *fileName = read_file(mtarptr, fileName_length);
+    char *file_content = read_file(mtarptr, sb.st_size);
 
-  FILE *fptr = fopen(fileName, "w");
-  if (!fptr)
-  {
-    perror("fopen() create file");
-    exit(1);
-  }
-  fwrite(file_content, sizeof(char), sb.st_size, fptr);
+    FILE *fptr = fopen(fileName, "w");
+    if (!fptr) {
+      perror("fopen() create file");
+      exit(1);
+    }
+    fwrite(file_content, sizeof(char), sb.st_size, fptr);
 
-  char c;
-  c = fgetc(fptr);
-  printf("%c\n", c);
-  if (feof(fptr))
-  {
-    extract(mtarname);
-  }
+    printf("extracted %s (%lu bytes)\n", fileName, sb.st_size);
 
+    fclose(fptr);
+    free(fileName);
+    free(file_content);
+  }
   fclose(mtarptr);
-  fclose(fptr);
-  free(fileName);
-  free(file_content);
 }
 
-int main(int argc, char **argv)
-{
-  if (argc < 2)
-  {
+int main(int argc, char **argv) {
+  if (argc < 2) {
     usage_err();
     exit(1);
   }
 
   /* ARCHIVE */
-  if (strcmp(argv[1], "-a") == 0)
-  {
-    if (argc < 4)
-    {
+  if (strcmp(argv[1], "-a") == 0) {
+    if (argc < 4) {
       fprintf(stderr,
               "myTar:\tarchive: ./myTar -a file.mtar file1 [files...]\n\n");
       exit(1);
@@ -161,23 +144,20 @@ int main(int argc, char **argv)
     check_extension(argv[2]);
 
     FILE *mtarptr = fopen(argv[2], "w");
-    if (!mtarptr)
-    {
+    if (!mtarptr) {
       perror("fopen()");
       exit(1);
     }
-    for (int i = 3; i < argc; i++)
-    {
+    for (int i = 3; i < argc; i++) {
       archive(argv[i], mtarptr);
     }
 
+    printf("myTar:\tarchive: success\n");
     fclose(mtarptr);
   }
   /*EXTRACT*/
-  else if (strcmp(argv[1], "-x") == 0)
-  {
-    if (argc < 3)
-    {
+  else if (strcmp(argv[1], "-x") == 0) {
+    if (argc < 3) {
       fprintf(stderr, "myTar:\textract: ./myTar -x file.mtar\n\n");
       exit(1);
     }
@@ -185,22 +165,19 @@ int main(int argc, char **argv)
     check_extension(argv[2]);
 
     FILE *tarptr = fopen(argv[2], "r");
-    if (!tarptr)
-    {
+    if (!tarptr) {
       perror("fopen()");
       exit(1);
     }
     extract(argv[2]);
   }
   /* HELP */
-  else if (strcmp(argv[1], "-h") == 0 || strcmp(argv[1], "--help") == 0)
-  {
+  else if (strcmp(argv[1], "-h") == 0 || strcmp(argv[1], "--help") == 0) {
     help();
     exit(0);
   }
   /* ERROR */
-  else
-  {
+  else {
     usage_err();
     exit(1);
   }
