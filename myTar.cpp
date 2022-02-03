19a3c50250690bff0f240194c4f65c10eff0f63d
Cameron Tredoux <camtredoux@gmail.com>
2022-02-03 11:58:40 -0700

    Added ifdefs for apple

--- myTar.cpp
+++ myTar.cpp
@@ -9,33 +9,40 @@
 
 using namespace std;
 
-void help() {
+void help()
+{
   printf("myTar:\t-a: archive\n\t\tfile.mtar: name of archive file\n\t\tfile1: "
          "must have at least one file to "
          "archive\n\t\tfiles: optional additional files to archive\n\t-x: "
          "extract\n\t\tfile.mtar: archived file to extract files from\n\n");
 }
 
-void usage_err() {
+void usage_err()
+{
   fprintf(stderr, "myTar:\tarchive: ./myTar -a file.mtar file1 "
                   "[files...]\n\textract: ./myTar -x file.mtar\n\n");
 }
 
-void file_err(const char *type) {
+void file_err(const char *type)
+{
   fprintf(stderr, "myTar: \t%s: incorrect file extension\n\n", type);
 }
 
-void check_extension(const char *fileName) {
+void check_extension(const char *fileName)
+{
   const char *fileNameExtension = strrchr(fileName, '.');
-  if (!fileNameExtension || strcmp(fileNameExtension, ".mtar") != 0) {
+  if (!fileNameExtension || strcmp(fileNameExtension, ".mtar") != 0)
+  {
     file_err("archive");
     exit(1);
   }
 }
 
-char *read_file(FILE *fptr, off_t size) {
+char *read_file(FILE *fptr, off_t size)
+{
   char *file_content = (char *)malloc(sizeof(char) * size);
-  if (fread(file_content, sizeof(char), size, fptr) < size) {
+  if (fread(file_content, sizeof(char), size, fptr) < size)
+  {
     perror("fread() file content");
     exit(1);
   }
@@ -43,20 +50,24 @@ char *read_file(FILE *fptr, off_t size) {
   return file_content;
 }
 
-void archive(const char *fileName, FILE *mtarptr) {
+void archive(const char *fileName, FILE *mtarptr)
+{
   FILE *fptr = fopen(fileName, "r");
-  if (!fptr) {
+  if (!fptr)
+  {
     perror("fopen()");
     exit(1);
   }
 
   struct stat sb;
-  if (stat(fileName, &sb) == -1) {
+  if (stat(fileName, &sb) == -1)
+  {
     perror("stat()");
     exit(1);
   }
 
-  if (S_ISREG(sb.st_mode)) {
+  if (S_ISREG(sb.st_mode))
+  {
     int fileName_length = strlen(fileName);
     fwrite(&fileName_length, sizeof(int), 1, mtarptr);
     fwrite(&sb, sizeof(struct stat), 1, mtarptr);
@@ -65,11 +76,17 @@ void archive(const char *fileName, FILE *mtarptr) {
     char *file_content = read_file(fptr, sb.st_size);
     fwrite(file_content, sizeof(char), sb.st_size, mtarptr);
 
+#ifdef __APPLE__
+    printf("archived %s (%lld bytes)\n", fileName, sb.st_size);
+#else
     printf("archived %s (%lu bytes)\n", fileName, sb.st_size);
+#endif
 
     free(file_content);
     fclose(fptr);
-  } else {
+  }
+  else
+  {
     fprintf(
         stderr,
         "myTar:\tarchive: incorrect file type (must be a regular file)\n\n");
@@ -77,28 +94,34 @@ void archive(const char *fileName, FILE *mtarptr) {
   }
 }
 
-void extract(const char *mtarname) {
+void extract(const char *mtarname)
+{
   FILE *mtarptr = fopen(mtarname, "r");
-  if (!mtarptr) {
+  if (!mtarptr)
+  {
     perror("fopen()");
     exit(1);
   }
 
   int count = 0;
-  while (1) {
+  while (1)
+  {
     struct stat sb;
 
     int fileName_length;
-    if (fread(&fileName_length, sizeof(int), 1, mtarptr) < 1) {
+    if (fread(&fileName_length, sizeof(int), 1, mtarptr) < 1)
+    {
       // if EOF, success
-      if (errno == 0) {
+      if (errno == 0)
+      {
         printf("myTar:\textract: success\n");
         exit(0);
       }
       perror("fread() filename length");
       exit(1);
     }
-    if (fread(&sb, sizeof(struct stat), 1, mtarptr) < 1) {
+    if (fread(&sb, sizeof(struct stat), 1, mtarptr) < 1)
+    {
       perror("fread() stat buffer");
       exit(1);
     }
@@ -107,7 +130,8 @@ void extract(const char *mtarname) {
     char *file_content = read_file(mtarptr, sb.st_size);
 
     FILE *fptr = fopen(fileName, "w");
-    if (!fptr) {
+    if (!fptr)
+    {
       perror("fopen() create file");
       exit(1);
     }
@@ -116,16 +140,25 @@ void extract(const char *mtarname) {
 
     fwrite(file_content, sizeof(char), sb.st_size, fptr);
 
+#ifdef __APPLE__
+    printf("extracted %s (%lld bytes)\n", fileName, sb.st_size);
+#else
     printf("extracted %s (%lu bytes)\n", fileName, sb.st_size);
+#endif
 
-    // struct utimbuf tb;
-    // tb.actime = sb.st_atim.tv_sec;
-    // tb.modtime = sb.st_mtim.tv_sec;
-    // if (utime(fileName, &tb) == -1) {
-    //   perror("utime()");
-    //   exit(1);
-    // }
-    // printf("atime: %lu, mtime: %lu\n", tb.actime, tb.modtime);
+    struct utimbuf tb;
+#ifdef __APPLE__
+    tb.actime = sb.st_atimespec.tv_sec;
+    tb.modtime = sb.st_mtimespec.tv_sec;
+#else
+    tb.actime = sb.st_atim.tv_sec;
+    tb.modtime = sb.st_mtim.tv_sec;
+#endif
+    if (utime(fileName, &tb) == -1)
+    {
+      perror("utime()");
+      exit(1);
+    }
 
     fclose(fptr);
     free(fileName);
@@ -134,15 +167,19 @@ void extract(const char *mtarname) {
   fclose(mtarptr);
 }
 
-int main(int argc, char **argv) {
-  if (argc < 2) {
+int main(int argc, char **argv)
+{
+  if (argc < 2)
+  {
     usage_err();
     exit(1);
   }
 
   /* ARCHIVE */
-  if (strcmp(argv[1], "-a") == 0) {
-    if (argc < 4) {
+  if (strcmp(argv[1], "-a") == 0)
+  {
+    if (argc < 4)
+    {
       fprintf(stderr,
               "myTar:\tarchive: ./myTar -a file.mtar file1 [files...]\n\n");
       exit(1);
@@ -151,11 +188,13 @@ int main(int argc, char **argv) {
     check_extension(argv[2]);
 
     FILE *mtarptr = fopen(argv[2], "w");
-    if (!mtarptr) {
+    if (!mtarptr)
+    {
       perror("fopen()");
       exit(1);
     }
-    for (int i = 3; i < argc; i++) {
+    for (int i = 3; i < argc; i++)
+    {
       archive(argv[i], mtarptr);
     }
 
@@ -163,8 +202,10 @@ int main(int argc, char **argv) {
     fclose(mtarptr);
   }
   /*EXTRACT*/
-  else if (strcmp(argv[1], "-x") == 0) {
-    if (argc < 3) {
+  else if (strcmp(argv[1], "-x") == 0)
+  {
+    if (argc < 3)
+    {
       fprintf(stderr, "myTar:\textract: ./myTar -x file.mtar\n\n");
       exit(1);
     }
@@ -172,19 +213,22 @@ int main(int argc, char **argv) {
     check_extension(argv[2]);
 
     FILE *tarptr = fopen(argv[2], "r");
-    if (!tarptr) {
+    if (!tarptr)
+    {
       perror("fopen()");
       exit(1);
     }
     extract(argv[2]);
   }
   /* HELP */
-  else if (strcmp(argv[1], "-h") == 0 || strcmp(argv[1], "--help") == 0) {
+  else if (strcmp(argv[1], "-h") == 0 || strcmp(argv[1], "--help") == 0)
+  {
     help();
     exit(0);
   }
   /* ERROR */
-  else {
+  else
+  {
     usage_err();
     exit(1);
   }
